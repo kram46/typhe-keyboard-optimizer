6c603d36e1c3818b708081e41c002c7159fcde44
kram46 <80665149+kram46@users.noreply.github.com>
2023-04-06 19:39:39 +0800

    Added iterating while fixed familarity and save to txt file

--- test4.py
+++ test4.py
@@ -25,29 +25,26 @@ for key, value in data.items():
     new_data[new_key] = value
 char_time = new_data
 
-# Define the extreme movements of the wrist and fingers for each keyslot
+# # Define the extreme movements of the wrist and fingers for each keyslot
 ergo_score = {
-    0: 2, 1: 2, 2: 2, 3: 2, 4: 2.5, 5: 3, 6: 2, 7: 2, 8: 2, 9: 2,
-    10: 0, 11: 0, 12: 0, 13: 0, 14: 2, 15: 2, 16: 0, 17: 0, 18: 0, 
-    19: 2, 20: 2, 21: 2, 22: 2, 23: 3.5, 24: 2, 25: 2
+    0: 10, 1: 7, 2: 7, 3: 4, 4: 4,
+    5: 4, 6: 2, 7: 2, 8: 2, 9: 0,
+    10: 0, 11: 0, 12: 0, 13: 0, 14: 0,
+    15: 0, 16: 0, 17: 0, 18: 2, 19: 2,
+    20: 2, 21: 4, 22: 4, 23: 4, 24: 7,
+    25: 7, 26: 7, 27: 10, 28: 10, 29: 10
 }
 
 # Define the QWERTY positions for each character
-# qwerty_pos = {
-#     'a': 0, 'b': 13, 'c': 14, 'd': 3, 'e': 4,
-#     'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9,
-#     'k': 10, 'l': 11, 'm': 15, 'n': 12, 'o': 16,
-#     'p': 17, 'q': 0, 'r': 2, 's': 3, 't': 5,
-#     'u': 7, 'v': 12, 'w': 1, 'x': 14, 'y': 8, 'z': 13
-# }
-
 qwerty_pos = {
-    'a': 10, 'b': 23, 'c': 21, 'd': 12, 'e': 2,
-    'f': 13, 'g': 14, 'h': 15, 'i': 7, 'j': 16,
-    'k': 17, 'l': 18, 'm': 25, 'n': 24, 'o': 8,
-    'p': 9, 'q': 0, 'r': 3, 's': 11, 't': 4,
-    'u': 6, 'v': 22, 'w': 1, 'x': 20, 'y': 5, 'z': 19
+    'a': 0, 'b': 13, 'c': 14, 'd': 3, 'e': 4,
+    'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9,
+    'k': 10, 'l': 11, 'm': 15, 'n': 12, 'o': 16,
+    'p': 17, 'q': 0, 'r': 2, 's': 3, 't': 5,
+    'u': 7, 'v': 12, 'w': 1, 'x': 14, 'y': 8, 'z': 13
 }
+
+
 # Create the Gurobi optimization model
 m = gp.Model("KeyboardLayoutOptimizer")
 
@@ -63,32 +60,41 @@ ergonomics = gp.quicksum(char_freq[i] * ergo_score[k] * char_slot[i,k] for i in 
 # Define the familiarity objective function
 familiarity = gp.quicksum(char_freq[i] * abs(qwerty_pos[i]-k) * char_slot[i,k] for i in characters for k in keyslots)
 
-w_performance = 0.6
-w_ergonomics = 0.2
-w_familiarity = 0.2
 
+w_familiarity = 0.1
+w_performance = 0.9
+w_ergonomics = 0.0
+finalLayout = ""
+with open("keyboard_layouts.txt", "w") as f:
+    while w_performance >= 0.0:
+        m.setObjective(performance * w_performance + w_ergonomics * ergonomics + w_familiarity * familiarity, gp.GRB.MINIMIZE)
 
-m.setObjective(performance * w_performance + w_ergonomics * ergonomics + w_familiarity * familiarity, gp.GRB.MINIMIZE)
+        for i in characters:
+            m.addConstr(gp.quicksum(char_slot[(i, k)] for k in keyslots) == 1, f"Assign {i}")
 
-for i in characters:
-    m.addConstr(gp.quicksum(char_slot[(i, k)] for k in keyslots) == 1, f"Assign {i}")
+        for k in keyslots:
+            m.addConstr(gp.quicksum(char_slot[(i, k)] for i in characters) <= 1, f"Key slot {k} is assigned to at most one character")
+        # Solve the problem using Gurobi's default solver
+        m.optimize()
 
-for k in keyslots:
-    m.addConstr(gp.quicksum(char_slot[(i, k)] for i in characters) <= 1, f"Key slot {k} is assigned to at most one character")
-# Solve the problem using Gurobi's default solver
-m.optimize()
+        # Print the status of the problem (should be 'Optimal')
+        #print("Status:", gp.GRB.status[m.status])
 
-# Print the status of the problem (should be 'Optimal')
-#print("Status:", gp.GRB.status[m.status])
-
-# Print the optimal keyboard layout
-print("Optimal Keyboard Layout:")
-for k in keyslots:
-    row = ""
-    for i in characters:
-        if char_slot[(i, k)].x == 1:
-            row += i
-    print(row)
+        # Print the optimal keyboard layout
+        w_sum = w_performance + w_ergonomics + w_familiarity
+        print("weighted sum = ", w_sum)
+        f.write(f"Optimal Keyboard Layout (w_performance={w_performance}, w_ergonomics={w_ergonomics}, w_familiarity={w_familiarity}):")
+        for k in keyslots:
+            row = ""
+            for i in characters:
+                if char_slot[(i, k)].x == 1:
+                    row += i
+            f.write(row)
+        f.write("\n")
+        w_performance -= 0.1
+        w_ergonomics += 0.1
+        w_performance = round(w_performance, 1)
+        w_ergonomics = round(w_ergonomics, 1)
 
 
 
